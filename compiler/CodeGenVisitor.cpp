313df2cf6746223f87fe04ba35bb728f94405053
Oskar Howe <oskar.j.howe@campus.tu-berlin.de>
2022-03-17 12:15:03 +0100

    pdf in repo, many comments

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -4,10 +4,12 @@
 #define MAIN "_main"
 #else
 #define MAIN "main"
-#endif
-// visite de l'axiome donc:
-//   On cale les prerequis du programme minimal et au milieu on visite les expressions entre
+#endif 
 /*
+visite de l'axiome donc:
+On cale les prerequis du programme minimal et au milieu on visite les expressions entre
+prints the basic layout into the .s file and starts the evaluation of the program by calling visitChildren(ctx).
+
 int main(){
     ...
     return 0;
@@ -23,66 +25,97 @@ main:
 antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx)
 {
     std::cout << ".globl    " << MAIN << "\n " << MAIN << ": \n 	pushq %rbp\n 	movq %rsp, %rbp\n\n";
-    visitChildren(ctx);
+    visitChildren(ctx); // not safe for use in visitors that modify the tree structure
     std::cout << "\n 	popq %rbp\n 	ret\n";
-    symbolTable.checkUse();
+    symbolTable.checkUse(); //warings for all unused variables
     return 0;
 }
+
+/*
+evaluates an expression like
+int var_1 = -9090;
+int var_1 = var_2;
+int var_1 = -9090, var_2 = var_3;
+int var_1, var_2, var_3 = -90909;
+
+remark when no variable is assigned to the varaible, the memoryspace can hold ANY value
+*/
 antlrcpp::Any CodeGenVisitor::visitSdecl(ifccParser::SdeclContext *ctx)
 {
-    std::string id = ctx->ID()->getText();
-    symbolTable.add(id,
-                    4,
-                    {ctx->ID()->getSymbol()->getLine(), ctx->ID()->getSymbol()->getCharPositionInLine()});
+    std::string varname = ctx->ID()->getText(); //get the name of the variable as string
+    symbolTable.add(varname,
+                    4, //in the futer we should consider the type of the variable, 4 is not always the right size
+                    {ctx->ID()->getSymbol()->getLine(), ctx->ID()->getSymbol()->getCharPositionInLine()}); //save the position in the program
 
-    if (ctx->rval() != nullptr)
+    if (ctx->rval() != nullptr) //we have an expression which assigns int var = CONST|ID;
     {
         if (ctx->rval()->CONST() != nullptr)
         {
-            std::cout << " 	movl	$" << stoi(ctx->rval()->CONST()->getText()) << ", -" << symbolTable.getOffset(id) << "(%rbp)\n";
+            //save the constant on the stack
+            std::cout << " 	movl	$" << stoi(ctx->rval()->CONST()->getText()) << ", -" << symbolTable.getOffset(varname) << "(%rbp)\n";
         }
         else
-        {
-            assign(ctx->rval()->ID()->getText(), id);
+        {   
+            //int varname = var_1;
+            //assign(var_1, varname);
+            assign(ctx->rval()->ID()->getText(), varname);
         }
     }
 
     return 0;
 }
-//visiteur de l'assignation
+/*
+evaluates an expression like 
+var_1 = -9902; 
+var_1 = var_2;
+*/
 antlrcpp::Any CodeGenVisitor::visitSassign(ifccParser::SassignContext *ctx)
 {
     std::string id = ctx->ID()->getText();
 
-    if (ctx->rval()->CONST() != nullptr)
+    if (ctx->rval()->CONST() != nullptr) //case var = -9902;
     {
-
+        //movl $"-9902", -"offset_of_variable(%rbp)"
         std::cout << " 	movl	$" << stoi(ctx->rval()->CONST()->getText()) << ", -" << symbolTable.getOffset(id) << "(%rbp)\n";
     }
-    else
+    else// case var_1 = var_2;
     {
-        assign(ctx->rval()->ID()->getText(), id);
+        //assign(var_1, valueof(var_2))
+        //!!!possible bug because value not id of first argument is passed!!!   
+        assign(ctx->rval()->ID()->getText(), id); 
     }
 
     return 0;
 }
-//visiteur du return
+/*
+this funtion is called for evaluating a return statement
+the return statement can either be a CONSTANT [0-9]+
+or a variable (ID) [a-zA-Z_][a-zA-Z0-9_]*
+*/
 antlrcpp::Any CodeGenVisitor::visitRet(ifccParser::RetContext *ctx)
 {
-    if (ctx->rval()->CONST() != nullptr)
+    if (ctx->rval()->CONST() != nullptr) // it's a constant
     {
+        //load the value of the constant expression into the %eax register which is used to return values
         std::cout << " 	movl	$" << ctx->rval()->CONST()->getText() << ", %eax\n";
     }
-    else
-    {
+    else    // return variable
+    {   
+        //get the position of the variable on the stack, and load the bytes from
+        //this register into the %eax register
         std::cout << " 	movl	-" << symbolTable.getOffset(ctx->rval()->ID()->getText()) << "(%rbp), %eax\n";
     }
 
     return 0;
 }
 
-void CodeGenVisitor::assign(std::string ids, std::string idd)
+/*
+evaluates an expression like variable_id_2 = variable_id_1;
+*/
+void CodeGenVisitor::assign(std::string id_1, std::string id_2)
 {
-    std::cout << " 	movl	-" << symbolTable.getOffset(ids) << "(%rbp), %eax\n";
-    std::cout << " 	movl	%eax, -" << symbolTable.getOffset(idd) << "(%rbp)\n";
+    //movl -offset(%rbp),%eax move value on this position in the stack into %eax
+    std::cout << " 	movl	-" << symbolTable.getOffset(id_1) << "(%rbp), %eax\n";
+    //move the new value in %eax into an other register
+    std::cout << " 	movl	%eax, -" << symbolTable.getOffset(id_2) << "(%rbp)\n";
 }
--- compiler/SymbolTable.cpp
+++ compiler/SymbolTable.cpp
@@ -2,23 +2,40 @@
 
 SymbolTable::SymbolTable(std::ostream *_out, std::ostream *_err) : out(_out), err(_err)
 {
+    /*
+    stores for a variable given by the string the position of the variable on the stack
+    by using the offest parameter.
+    eg: int var --> offsetMap->emplace(var, 4)
+    eg:char foo --> offsetMap->emplace(foo, 1)
+    */
     offsetMap = new std::map<std::string, size_t>();
+    /*
+    stores for each variable given by a string the linenumber in the file.c
+    pos is defined in the .h file with a line and a colum attribute.
+    */
     posMap = new std::map<std::string, pos>();
     usedMap = new std::map<std::string, bool>();
-
+    /*
+    points to the next free block of memory of the stack
+    */
     offset = 0;
 }
 
+/*
+constructor creates new structs
+*/
 SymbolTable::~SymbolTable()
 {
     delete offsetMap;
     delete posMap;
     delete usedMap;
 }
-
+/*
+stores the name, size of the new variable and saves it's offset on the stack
+*/
 void SymbolTable::add(std::string id, size_t size, pos pos)
 {
-    offset += size;
+    offset += size;  // strange because now the offset of a variable points to the end of the variable...?
     auto p = offsetMap->emplace(id, offset);
     if (!p.second)
     {
@@ -32,17 +49,28 @@ void SymbolTable::add(std::string id, size_t size, pos pos)
     usedMap->emplace(id, false);
 }
 
+/*
+sets the variable to used which means that a value has been assiged to it
+*/
 void SymbolTable::used(std::string id)
 {
     usedMap->at(id) = true;
 }
 
+/*
+retruns the offset on the stack of the given variable
+stops the program if tha variable is not declared
+*/
 int SymbolTable::getOffset(std::string id)
 {
     checkVar(id);
     return offsetMap->at(id);
 }
 
+/*
+checks if the variable that should be accessed is declared, 
+stops the program if tha variable is not declared
+*/
 void SymbolTable::checkVar(std::string id)
 {
     if (offsetMap->count(id) == 0)
@@ -52,11 +80,15 @@ void SymbolTable::checkVar(std::string id)
     }
 }
 
+/*
+checks if any variable has never been used 
+returs a warning if not
+*/
 void SymbolTable::checkUse()
 {
     for (auto v : *usedMap)
     {
-        if (!v.second)
+        if (!v.second) //second argument in <string,bool> is false, so not used
         {
             *err << "Warning: variable " << v.first << " is never used" << std::endl;
             *err << "\t Declared at line " << posMap->at(v.first).line << ":" << posMap->at(v.first).column << std::endl;
