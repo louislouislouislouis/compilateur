17b2c9227aa62a3c5f80cbb1e4bb0af066416348
Lombard Louis <louis.raphael.lombard@gmail.com>
2022-03-29 08:56:28 +0200

    hihi

--- compiler/IRGenVisitor.h
+++ compiler/IRGenVisitor.h
@@ -8,10 +8,15 @@
 using namespace std;
 
 class  IRGenVisitor : public ifccBaseVisitor {
-	public:
-		virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override ;
 	private:
 		vector<CFG*> instructionsList;
+		SymbolTable symbolTable;
+	public:
+		IRGenVisitor(std::ostream &out = std::cout, std::ostream &err = std::cerr) : symbolTable(out, err){};
+		virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override ;
+		virtual antlrcpp::Any visitSdecl(ifccParser::SdeclContext *ctx) override;
+
+	
 
 };
 
